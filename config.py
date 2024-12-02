3146b8a579d132c449c257cdca8203e7e1d22bf2
tosteg87 <149069989+tosteg87@users.noreply.github.com>
2024-12-02 15:06:16 +0300

    rey

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-SQLALCHEMY_DATABASE_URI = 'postgresql://football_reminder_user:wgIKWuSocl8GMPWm2ZDkt2WH6XhNUX7d@dpg-ct43r6pu0jms73a5fus0-a/football_reminder'
+SQLALCHEMY_DATABASE_URI = 'postgresql://football_reminder_user:wgIKWuSocl8GMPWm2ZDkt2WH6XhNUX7d@dpg-ct43r6pu0jms73a5fus0-a.frankfurt-postgres.render.com/football_reminder'
 
 SECRET_KEY = 'AAFptTVEoPltHwMRmRo2ZiG2sAetXc-96eU'
