5d5ecfd0a7a7d7db893ac77c4f5eef9f7dde4aed
tosteg87 <149069989+tosteg87@users.noreply.github.com>
2024-12-02 14:41:41 +0300

    rer

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,11 @@ import time
 from threading import Thread
 import os
 
-port=int(os.getenv('PORT'), 10000)
+#port=int(os.getenv('PORT'), 10000)
 
 def start():
-    app.run(host='0.0.0.0', port=port, debug=True)
+    #app.run(host='0.0.0.0', port=port, debug=True)
+    app.run()
 
 if __name__ == '__main__':
     t = Thread(target=start)
