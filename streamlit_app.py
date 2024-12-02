f01aca3017b4668ae2d1abbc27fa952657dd6a08
tosteg87 <149069989+tosteg87@users.noreply.github.com>
2024-12-02 16:04:07 +0300

    sdf

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,6 +10,6 @@ def start():
     app.run(debug=True)
 
 if __name__ == '__main__':
-    t = Thread(target=start)
-    t.start()
-    #app.run(host='0.0.0.0', port=port)
+    #t = Thread(target=start)
+    #t.start()
+    app.run(debug=Truesd)
